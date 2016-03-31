b27cacab87eab6cef14c28c7cda580e184f808d4
Drew Kalina <drew.kalina@gmail.com>
2016-03-31 00:36:06 -0700

    Cleaning up indentation and formatting

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -16,24 +16,24 @@ public class Etl {
 		scores.put("f", 4);
 		scores.put("g", 2);
 		scores.put("h", 4);
-		scores.put(	"i", 1);
+		scores.put("i", 1);
 		scores.put("j", 8);
-		scores.put(	"k", 5);
-		scores.put(	"l", 1);
-		scores.put(	"m", 3);
-		scores.put(	"n", 1);
-		scores.put(	"o", 1);
-		scores.put(	"p", 3);
-		scores.put(	"q", 10);
-		scores.put(	"r", 1);
-		scores.put(	"s", 1);
-		scores.put(	"t", 1);
-		scores.put(	"u", 1);
-		scores.put(	"v", 4);
-		scores.put(	"w", 4);
-		scores.put(	"x", 8);
-		scores.put(	"y", 4);
-		scores.put(	"z", 10);
+		scores.put("k", 5);
+		scores.put("l", 1);
+		scores.put("m", 3);
+		scores.put("n", 1);
+		scores.put("o", 1);
+		scores.put("p", 3);
+		scores.put("q", 10);
+		scores.put("r", 1);
+		scores.put("s", 1);
+		scores.put("t", 1);
+		scores.put("u", 1);
+		scores.put("v", 4);
+		scores.put("w", 4);
+		scores.put("x", 8);
+		scores.put("y", 4);
+		scores.put("z", 10);
 	}
 
 	public Map<String, Integer> transform(Map<Integer, List<String>> old) {
--- space-age/src/main/java/SpaceAge.java
+++ space-age/src/main/java/SpaceAge.java
@@ -4,47 +4,47 @@ public class SpaceAge {
 	private double EARTH_ORBIT = 31557600;
 	private double MERCURY_ORBIT =  0.2408467 * 31557600;
 	private double VENUS_ORBIT = 0.61519726 *  31557600;
-    private double MARS_ORBIT = 1.8808158 *  31557600;
-    private double JUPITER_ORBIT = 11.862615 *  31557600;
-    private double SATURN_ORBIT = 29.447498 *  31557600;
-    private double URANUS_ORBIT = 84.016846 *  31557600;
-    private double NEPTUNE_ORBIT = 164.79132 *  31557600;
-	
-    public SpaceAge(double seconds){
-    	this.seconds = seconds;
-    }
-    
-    public double getSeconds(){
-    	return this.seconds;
-    }
-    
-    public double onEarth(){
-    	return this.seconds / EARTH_ORBIT;
-    }
-    public double onMercury(){
-    	return this.seconds / MERCURY_ORBIT;
-    }
-    
-    public double onVenus(){
-    	return this.seconds / VENUS_ORBIT;
-    }
-    
-    public double onMars(){
-    	return this.seconds / MARS_ORBIT;
-    }
-    
-    public double onJupiter(){
-    	return this.seconds / JUPITER_ORBIT;
-    }
-    
-    public double onSaturn(){
-    	return this.seconds / SATURN_ORBIT;
-    }
-    
-    public double onUranus(){
-    	return this.seconds / URANUS_ORBIT;
-    }
-    public double onNeptune(){
-    	return this.seconds / NEPTUNE_ORBIT;
-    }
+	private double MARS_ORBIT = 1.8808158 *  31557600;
+	private double JUPITER_ORBIT = 11.862615 *  31557600;
+	private double SATURN_ORBIT = 29.447498 *  31557600;
+	private double URANUS_ORBIT = 84.016846 *  31557600;
+	private double NEPTUNE_ORBIT = 164.79132 *  31557600;
+
+	public SpaceAge(double seconds){
+		this.seconds = seconds;
+	}
+
+	public double getSeconds(){
+		return this.seconds;
+	}
+
+	public double onEarth(){
+		return this.seconds / EARTH_ORBIT;
+	}
+	public double onMercury(){
+		return this.seconds / MERCURY_ORBIT;
+	}
+
+	public double onVenus(){
+		return this.seconds / VENUS_ORBIT;
+	}
+
+	public double onMars(){
+		return this.seconds / MARS_ORBIT;
+	}
+
+	public double onJupiter(){
+		return this.seconds / JUPITER_ORBIT;
+	}
+
+	public double onSaturn(){
+		return this.seconds / SATURN_ORBIT;
+	}
+
+	public double onUranus(){
+		return this.seconds / URANUS_ORBIT;
+	}
+	public double onNeptune(){
+		return this.seconds / NEPTUNE_ORBIT;
+	}
 }
